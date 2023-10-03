61a897639188e853c06c32f63b11b69b2aec6c2b
rhardie1 <146765130+rhardie1@users.noreply.github.com>
2023-10-03 16:32:06 -0400

    Add files via upload

---
+++ main.bat
@@ -0,0 +1,4 @@
+rem This is main.bat file which calls 
+rem flyerscan.exe with input parameters
+
+flyerscan "flyerscan_output_0003" ".\LIDC-IDRI-0003\01-01-2000-NA-NA-94866\3000611.000000-NA-03264"
