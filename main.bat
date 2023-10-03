a0ac1e7d5efb9d922f007c155129ff67d7ff334f
rhardie1 <146765130+rhardie1@users.noreply.github.com>
2023-10-03 16:17:43 -0400

    Add files via upload

---
+++ main.bat
@@ -0,0 +1,4 @@
+rem This is main.bat file which calls 
+rem filename.exe with input parameters
+
+flyerscan "test4" "E:\Medical\CCHMC\FIRST_PHASE\FIRST_PHASE_SCANS\Trout_Lung_029\3_Trout_Lung_029_11528.2020091908362"
