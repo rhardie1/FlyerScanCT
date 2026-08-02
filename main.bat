a8d2f948905701a97aa97df2ff938a88e2db454d
Russell Hardie <146765130+rhardie1@users.noreply.github.com>
2026-08-02 14:20:36 -0400

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,4 +1,4 @@
 rem This is main.bat file which calls 
 rem flyerscan.exe with input parameters
 
-flyerscan "flyerscan_output_0003" ".\LIDC-IDRI-0003\01-01-2000-NA-NA-94866\3000611.000000-NA-03264"
+"C:\Program Files\flyerscan\application\flyerscan.exe" "deep_flyerscan_output_0003" "E:\Medical\LIDC-IDRI\manifest-1600709154662\LIDC-IDRI\LIDC-IDRI-0003\01-01-2000-NA-NA-94866\3000611.000000-NA-03264"
